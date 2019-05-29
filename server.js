02ecf9d9d5cc144fb79993e9bf4a72297252e0cb
ronsbons <ronnilouie@gmail.com>
2019-05-29 16:27:05 -0700

    started server file

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// require routes
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }) );
+app.use(bodyParser.json());
+app.use(cors());
+
+// backup cors allowance
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+// app.use routes
+
+
+// serve the 'public' folder in the frontend which has 'index.html', which index.js puts components into
+// app.use(express.static('public'));
+
+
+// test connection response to send to front end when it connects to backend
+app.get('/', (request, response) => response.send('hello, backend'));
+
+
+app.listen(process.env.PORT || 3001, () =>
+  console.log('Listening on port 3001 :)')
+);
