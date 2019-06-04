b04c78b4a3f728cc8440876c681bbd890d46eb08
ronsbons <ronnilouie@gmail.com>
2019-06-03 17:26:10 -0700

    adds connection string, unsuccessful storing it in .env

--- models/index.js
+++ models/index.js
@@ -1,18 +1,20 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://ronsbons:40zpSlcvuNJbwK0p@ronni-post-wdi-ex4lk.azure.mongodb.net/test?retryWrites=true&w=majority';
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = 'mongodb+srv://ronsbons:40zpSlcvuNJbwK0p@ronni-post-wdi-ex4lk.azure.mongodb.net/test?retryWrites=true&w=majority';
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  // [] need to change db and collection names
-  const collection = client.db('test').collection('devices');
-  // perform actions on the collection object
-  client.close();
-});
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   // [] need to change db and collection names
+//   const collection = client.db('test').collection('devices');
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ronsbons:40zpSlcvuNJbwK0p@ronni-post-wdi-ex4lk.azure.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+const connectionString = 'mongodb+srv://ronsbons:Alb9BHg9@ronni-post-wdi-ex4lk.azure.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(connectionString, { useNewUrlParser: true });
 
 
 module.exports.Ingredients = require('./ingredientModel.js');
